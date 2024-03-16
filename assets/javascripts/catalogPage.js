6f8772b1de1d1ea445efe1e85eadec23db519929
sevincovskienode <sevincovsky@gmail.com>
2024-03-16 23:05:31 +0400

    edit swiper js for catalog

--- assets/javascripts/catalogPage.js
+++ assets/javascripts/catalogPage.js
@@ -9,6 +9,10 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 let swiperWrapper = document.querySelector("#all_swiper_books");
+ let all_swiper_books = document.getElementById("all_swiper_books");
+ let besteller_swiper_books= document.getElementById("bestseller_swiper_books");
+ let new_swiper_books = document.getElementById("new_swiper_books");
+
 onValue(ref(dataBase, "/books"), (response) => {
   const result = response.val();
   swiperWrapper.innerHTML = "";
@@ -17,7 +21,7 @@ onValue(ref(dataBase, "/books"), (response) => {
     swiperWrapper.innerHTML += `
         <div class="swiper-slide">
        <div class="catalog_swiper_card">
-       <span>New</span>
+        <span>New</span>
        <img class="swiper_img" src="${result[book].imageURL}" alt="">
        <h3 class="swiper_book">${result[book].title}</h3>
        <button class="swiper_btn">Read More</button>
@@ -27,7 +31,7 @@ onValue(ref(dataBase, "/books"), (response) => {
 });
 
 function swiper() {
-  const swiper_books = new Swiper(".swiper.swiper_catalog", {
+  const swiper_all = new Swiper(".swiper.swiper_catalog", {
     // Optional parameters
     slidesPerView: 5,
     direction: "horizontal",
@@ -63,6 +67,82 @@ function swiper() {
       },
     },
   });
+  
+
+  const besteller_swiper  = new Swiper (".swiper.besteller_swiper_books",{
+    // Optional parameters
+    slidesPerView: 5,
+    direction: "horizontal",
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    autoplay: {
+      delay: 2000,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      767: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+
+  });
+
+  const new_swiper = new Swiper(".swiper.new_swiper_books",{
+    // Optional parameters
+    slidesPerView: 5,
+    direction: "horizontal",
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    autoplay: {
+      delay: 2000,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      767: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  })
 }
 
 setTimeout(swiper, 4000);
