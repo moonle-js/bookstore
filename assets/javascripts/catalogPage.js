f5ff734d9718eb82fbb3f12e9d28af67d3f27081
Abbasov <MABBA952@marriott.com>
2024-03-18 11:37:02 +0400

    Swiper changes

--- assets/javascripts/catalogPage.js
+++ assets/javascripts/catalogPage.js
@@ -18,8 +18,8 @@ const swiper_all = new Swiper(".swiper.swiper_catalog", {
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".all_swiper-button-next",
+    prevEl: ".all_swiper-button-prev",
   },
 
   autoplay: {
@@ -48,185 +48,181 @@ const swiper_all = new Swiper(".swiper.swiper_catalog", {
   },
 });
 
-const besteller_swiper = new Swiper(".swiper.besteller_swiper_books", {
-  // Optional parameters
-  slidesPerView: 5,
-  direction: "horizontal",
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 
-  autoplay: {
-    delay: 2000,
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1.5,
-      spaceBetween: 20,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    // when window width is >= 640px
-    767: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    1200: {
-      slidesPerView: 5,
-      spaceBetween: 20,
-    },
-  },
-});
+// firebase for all books
 
-const new_swiper = new Swiper(".swiper.new_swiper_books", {
-  // Optional parameters
-  slidesPerView: 5,
-  direction: "horizontal",
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  autoplay: {
-    delay: 2000,
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1.5,
-      spaceBetween: 20,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    // when window width is >= 640px
-    767: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    1200: {
-      slidesPerView: 5,
-      spaceBetween: 20,
-    },
-  },
-});
-
-/* ======================== Firebase Methods ========================= */
-
-let new_swiper_books = document.getElementById("new_swiper_book");
-let swiperWrapper = document.getElementById("all_swiper_books");
-let besteller_swiper_books = document.getElementById("besteller_swiper_books");
-
-onValue(ref(dataBase, "books"), (response) => {
-  const result = response.val();
-  swiperWrapper.innerHTML = "";
-
-  for (const book in result) {
-    swiperWrapper.innerHTML += `
+onValue(ref(dataBase, "books"), data => {
+  if(data.exists()){
+    document.querySelector('#all_swiper_books').innerHTML = ""
+    for(let keys in data.val()){
+      console.log('salam')
+        document.querySelector('#all_swiper_books').innerHTML += `
         <div class="swiper-slide">
-       <div class="catalog_swiper_card">
-       <img class="swiper_img" src="${result[book].imageURL}" alt="">
-       <h3 class="swiper_book">${result[book].title}</h3>
-       <button class="swiper_btn">Read More</button>
-       </div>
-     </div> `;
+          <div class="catalog_swiper_card">
+          <span>New</span>
+          <img class="swiper_img" src="${data.val()[keys].imageURL}" alt="">
+          <h3 class="swiper_book">${data.val()[keys].title}</h3>
+          <button class="swiper_btn">Read More</button>
+          </div>
+        </div> `
+        swiper_all.update()
+    }
   }
-  swiper_all.update();
-});
+})
 
-
-
-function convertData(d) {
-  const newData = Object.entries(d);
-  const myNewData = newData.map((arr) => {
-    const newObj = {
-      id: arr[0],
-      ...arr[1],
-    };
-    return newObj;
-  });
-  return myNewData;
+function showSelectedCategory(categoryName){
+  get(ref(dataBase, `bookTypes/${categoryName}`)).then(data => {
+    if(data.exists()){
+      document.querySelector('#category_swiper').innerHTML = `
+          <h2 class="catalog_title">${data.val()}</h2>
+        `
+      onValue(ref(dataBase, 'books/'), result => {
+        if(result.exists()){
+          document.querySelector('#selected_swiper_books').innerHTML = ""
+          for(let key in result.val()){
+            if(result.val()[key].category == categoryName){
+              document.querySelector('#selected_swiper_books').innerHTML += `
+              <div class="swiper-slide">
+                <div class="catalog_swiper_card">
+                <span>New</span>
+                <img class="swiper_img" src="${result.val()[key].imageURL}" alt="">
+                <h3 class="swiper_book">${result.val()[key].title}</h3>
+                <button class="swiper_btn">Read More</button>
+                </div>
+              </div> 
+              `
+              selected_release_swiper.update()
+            }
+          }
+        }
+      })
+    }
+  })
 }
 
+// Show All Categories from Firebase
+function getCategories(){
+  onValue(ref(dataBase, 'bookTypes/'), result => {
+    if(result.exists()){
+      document.querySelector('#catalog_categories').innerHTML = ''
+      for(let key in result.val()){
+        document.querySelector('#catalog_categories').innerHTML += `
+          <li>
+            <button class="showThisCategory" id="${result.val()[key]}">${result.val()[key]}</button>
+          </li>
+        `
+        document.querySelectorAll('.showThisCategory').forEach(function(item){
+          item.addEventListener('click', function(e){
+            window.scroll(0, 1000)
+            console.log('salam')
+            showSelectedCategory(`${this.id}`)
+          })
+        })
+      }
+    }
+  })
+}
+getCategories()
 
 
-onValue(ref(dataBase, "books"), (response) => {
+// ++++++++++++++++++++++++++++Swiper for new books ++++++++++++++++++++++++++++
 
-  const result = convertData(response.val());
 
-  let filtered_data = result.filter((item) => {
-    return item.new === "true";
-  });
+const new_release_swiper = new Swiper(".swiper.new_swiper_catalog", {
+  // Optional parameters
+  slidesPerView: 5,
+  direction: "horizontal",
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".new_swiper-button-next",
+    prevEl: ".new_swiper-button-prev",
+  },
 
-  let datalist = filtered_data
-    .map((item) => {
-      return `
-    <div class="swiper-slide">
-       <div class="catalog_swiper_card">
-       <span>New</span>
-       <img class="swiper_img" src="${item.imageURL}" alt="">
-       <h3 class="swiper_book">${item.title}</h3>
-       <button class="swiper_btn">Read More</button>
-       </div>
-     </div> `;
-    })
-    .join("");
-
-  new_swiper_books.innerHTML = datalist;
-  new_swiper.update();
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
 });
 
-onValue(ref(dataBase, "books"), (response) => {
+// Adding new releases
+onValue(ref(dataBase, "books/"), data => {
+  if(data.exists()){
 
-  const result = convertData(response.val());
+    document.querySelector('#new_swiper_books').innerHTML = ""
+    for(let keys in data.val()){
+      if(data.val()[keys].new == "true"){
+        
+        document.querySelector('#new_swiper_books').innerHTML += `
+        <div class="swiper-slide">
+          <div class="catalog_swiper_card">
+          <span>New</span>
+          <img class="swiper_img" src="${data.val()[keys].imageURL}" alt="">
+          <h3 class="swiper_book">${data.val()[keys].title}</h3>
+          <button class="swiper_btn">Read More</button>
+          </div>
+        </div> `
+        new_release_swiper.update();
+      }
+    }
+  }
+})
 
-  let filtered_data = result.filter((item) => {
-    return item.counter >= 15;
-  });
 
-  let datalist = filtered_data
-    .map((item) => {
-      return `
-    <div class="swiper-slide">
-       <div class="catalog_swiper_card">
-       <span>New</span>
-       <img class="swiper_img" src="${item.imageURL}" alt="">
-       <h3 class="swiper_book">${item.title}</h3>
-       <button class="swiper_btn">Read More</button>
-       </div>
-     </div> `;
-    })
-    .join("");
+// Categories swiper
+const selected_release_swiper = new Swiper(".swiper.selected_swiper_catalog", {
+  // Optional parameters
+  slidesPerView: 5,
+  direction: "horizontal",
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".selected_swiper-button-next",
+    prevEl: ".selected_swiper-button-prev",
+  },
 
-  besteller_swiper_books.innerHTML = datalist;
-  besteller_swiper.update();
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
 });
-
-// onValue(ref(dataBase, "books"), data => {
-//   if(data.exists()){
-//     for(let keys in data.val()){
-//       if(data.val()[keys].new == "true"){
-//         document.querySelector('#swiper').innerHTML += `
-//         <div class="swiper-slide">
-//           <div class="catalog_swiper_card">
-//           <span>New</span>
-//           <img class="swiper_img" src="${data.val()[keys].imageURL}" alt="">
-//           <h3 class="swiper_book">${data.val()[keys].title}</h3>
-//           <button class="swiper_btn">Read More</button>
-//           </div>
-//         </div> `
-//       }
-//     }
-//   }
-// })
