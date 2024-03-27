7544567a28842dc5a6c46fb6a21c68e157c3aa2f
Mahammad <mehemmedabbasov123@gmail.com>
2024-03-27 18:36:27 +0400

    Delete inspect

--- assets/javascripts/adminPanel.js
+++ assets/javascripts/adminPanel.js
@@ -20,7 +20,7 @@ function detectUser(valueFromPrompt, passwordFromPrompt){
                 }
 
             if(defValue == 0){
-                alert('user netu')
+                alert('User is not defined')
             }else{
                 document.querySelector('#mainContentForAdmin').innerHTML = `
                 <div id="adminMainPanel">
@@ -459,28 +459,6 @@ window.addEventListener('keyup', function(e){
     }
 })
 // end of detecting admins
-
-
-
-
-// document.addEventListener('contextmenu', (e) => e.preventDefault());
-
-// function ctrlShiftKey(e, keyCode) {
-//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-// }
-
-// document.onkeydown = (e) => {
-//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-//   if (
-//     event.keyCode === 123 ||
-//     ctrlShiftKey(e, 'I') ||
-//     ctrlShiftKey(e, 'J') ||
-//     ctrlShiftKey(e, 'C') ||
-//     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-//   )
-//     return false;
-// };
-
 
 
 
